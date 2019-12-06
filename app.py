d23ef1d279029a1b3816920164c6f07b1f3e9122
Adheesh Juvekar <juvekaradheesh@gmail.com>
2019-12-05 23:49:04 -0500

    add content

--- app.py
+++ app.py
@@ -11,5 +11,21 @@ app.config.from_object('config')
 def index():
     return render_template("index.html")
 
+@app.route('/what')
+def what():
+    return render_template("what.html")
+
+@app.route('/how')
+def how():
+    return render_template("how.html")
+
+@app.route('/findings')
+def findings():
+    return render_template("findings.html")
+
+@app.route('/papers')
+def conclusion():
+    return render_template("papers.html")
+
 if __name__ == '__main__':
     app.run()
